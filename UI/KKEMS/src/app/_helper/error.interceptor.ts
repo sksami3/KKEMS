156cbce146c44e9b6bd75bf3ed519429917b042d
sksami3 <sksami4456@gmail.com>
2022-06-25 15:39:52 +0600

    design changed

--- UI/KKEMS/src/app/_helper/error.interceptor.ts
+++ UI/KKEMS/src/app/_helper/error.interceptor.ts
@@ -5,26 +5,30 @@ import { catchError } from 'rxjs/operators';
 import { AuthenticationService } from '../_service/authentication.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
     constructor(private authenticationService: AuthenticationService,
-                private router : Router,
-                private tostr : ToastrService) { }
+        private router: Router,
+        private tostr: ToastrService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
+        return next.handle(request).pipe(tap(() => console.log() ),
+        catchError(err => {
             if ([401, 403].indexOf(err.status) !== -1) {
                 // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
                 this.authenticationService.logout();
                 //this.router.navigate(["Auth/login"]);
             }
 
-            const error = err.error.message || err.statusText;
-            
-            this.tostr.error(err.error);
-            return throwError(error);
+            //const error = err.error.message || err.statusText;
+            if (err['error']) {
+                this.tostr.error(err['error']);
+            }
+
+            return throwError(null);
         }))
     }
 }
